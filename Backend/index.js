69956f6099d94c6ededda79802aa29fb2d50ff85
Priyanshu Chaurasiya <sainty@Priyanshus-MacBook-Air.local>
2024-04-15 18:43:03 +0530

    minor fixes

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,41 @@
-const connectToMongo = require('./db');
-const express = require('express');
-const cors = require('cors');
+// index.js
 
-const app = express();
+const express = require('express');
+const connectToMongo = require('./db');
+const cors = require('cors');
+const authRouter = require('./routes/auth');
+const notesRouter = require('./routes/notes');
+
+// Connect to MongoDB
 connectToMongo();
 
+const app = express();
 const port = process.env.PORT || 5001;
 
-// CORS configuration
+// CORS Configuration
 const corsOptions = {
   origin: 'https://safe-memo.web.app', // Allow requests only from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
 };
 
-app.use(cors(corsOptions)); // Use cors middleware
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
+// Middleware for parsing JSON requests
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+// Routes
+app.use('/api/auth', authRouter);
+app.use('/api/notes', notesRouter);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
